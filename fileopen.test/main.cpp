dbe09cebae535f928c3c0b9601e485154dc739d6
0xbadfca11 <0xbadfca11@gmail.com>
2016-08-05 20:50:44 +0900

    Add fileopen test

---
+++ fileopen.test/main.cpp
@@ -0,0 +1,31 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <cstdio>
+#include <vector>
+#include <experimental/filesystem>
+#include "../fileopen.hpp"
+
+bool operator==(const FILE_ID_128& l, const FILE_ID_128& r)
+{
+	return memcmp(&l.Identifier, &r.Identifier, sizeof(FILE_ID_128::Identifier)) == 0;
+}
+FILE_ID_128 GetFileID(HANDLE f)
+{
+	FILE_ID_INFO file_id;
+	GetFileInformationByHandleEx(f, FileIdInfo, &file_id, sizeof file_id);
+	return file_id.FileId;
+}
+int wmain(int argc, PWSTR argv[])
+{
+	std::vector<FILE_ID_128> ids;
+	for (int i = 1; i < argc; i++)
+	{
+		FILE_ID_128 id = GetFileID(OpenFileCaseSensitive(std::experimental::filesystem::system_complete(argv[i]).c_str()));
+		for (int j = _countof(FILE_ID_128::Identifier) - 1; j >= 0; --j)
+			printf("%02x", id.Identifier[j]);
+		printf("\t%ls\n", argv[i]);
+		if (std::find(std::cbegin(ids), std::cend(ids), id) != std::cend(ids))
+			puts("Identical file id found.");
+		ids.push_back(id);
+	}
+}
